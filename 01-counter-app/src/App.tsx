387a1328c21987f4978342ff3e192342088ab61e
Denis Ulate <92893770+DenisUlate@users.noreply.github.com>
2025-09-22 15:01:10 -0600

    feat: initialize counter app with React, TypeScript, and Tailwind CSS
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite configuration for the project.
    - Created main application structure with App component.
    - Integrated Tailwind CSS for styling.
    - Set up TypeScript configuration files for app and node.
    - Added SVG assets for branding.
    - Implemented basic styling in index.css.

---
+++ 01-counter-app/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="bg-zinc-900 text-zinc-50 min-h-screen flex flex-col items-center justify-center">
+			<h1>Mi Counter App</h1>
+		</div>
+	);
+}
+
+export default App;
